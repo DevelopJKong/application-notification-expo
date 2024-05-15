c9c7f3ff635970e442463a4595b27ab0b4c0b970
cafe small house <ybaram40@gmail.com>
2024-05-15 23:03:58 +0900

    patches 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 
+import Constants from 'expo-constants';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
-import { Button, Platform, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Button, NativeBaseProvider } from 'native-base';
+import { Platform, Text, View } from 'react-native';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -36,7 +39,13 @@ async function registerForPushNotificationsAsync() {
       alert('Failed to get push token for push notification!');
       return;
     }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
+
+    console.log(Constants?.expoConfig?.extra?.eas?.projectId);
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: Constants?.expoConfig?.extra?.eas?.projectId || 'notification-app',
+      })
+    ).data;
     console.log(token);
   } else {
     alert('Must use physical device for Push Notifications');
@@ -55,28 +64,35 @@ async function registerForPushNotificationsAsync() {
 }
 
 export default function App() {
+  // ! 기본 state 모음
   const [expoPushToken, setExpoPushToken] = useState<string>('');
   const [notification, setNotification] = useState<any>(false);
 
+  // ! useRef 모음
   const notificationListener = useRef<any>(null);
   const responseListener = useRef<any>(null);
+
+  const router = useRouter();
+
+  // ! useEffect 모음
   useEffect(() => {
     registerForPushNotificationsAsync().then((token) => {
+      console.log(token);
       if (!token) return;
-      fetch('http://localhost:8000/token', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          token: {
-            value: token,
-          },
-        }),
-      })
-        .then(() => console.log('send!'))
-        .catch((err) => console.log(err));
+      // fetch('http://localhost:8000/token', {
+      //   method: 'POST',
+      //   headers: {
+      //     Accept: 'application/json',
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({
+      //     token: {
+      //       value: token,
+      //     },
+      //   }),
+      // })
+      //   .then(() => console.log('send!'))
+      //   .catch((err) => console.log(err));
       setExpoPushToken(token);
     });
 
@@ -97,25 +113,36 @@ export default function App() {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title='Press to schedule a notification'
-        onPress={async () => {
-          await schedulePushNotification();
+    <NativeBaseProvider>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
         }}
-      />
-    </View>
+      >
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Title: {notification && notification.request.content.title} </Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        </View>
+
+        <Button
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        >
+          <Text>Press to schedule a notification</Text>
+        </Button>
+        <Button
+          onPress={() => {
+            router.push('/setting');
+          }}
+        >
+          <Text>Setting</Text>
+        </Button>
+      </View>
+    </NativeBaseProvider>
   );
 }
